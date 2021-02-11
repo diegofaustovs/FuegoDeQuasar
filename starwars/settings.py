3f8c4fff8dde89683a6c6f511ba6e3953fc7f75a
dvanegas <diego.vanegas@blackboard.com>
2021-02-10 19:52:40 -0500

    Set prod mode

--- starwars/settings.py
+++ starwars/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_dh9y%r@4uqgn34hwt)1j=s_b4j#^wb$vv=pdwre#y)x-isl1x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
